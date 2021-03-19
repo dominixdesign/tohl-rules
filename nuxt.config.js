022f9da79944f7648b0191cde432375d24e2faf0
Dominik Sander <dominik.sander@googlemail.com>
2021-03-19 20:13:31 +0100

    url config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,9 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   docs: {
     primaryColor: '#0061AF'
+  },
+  target: 'static',
+  router: {
+    base: '/tohl-rules/'
   }
 })
